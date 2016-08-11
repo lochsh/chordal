e5bedd044673f85f00ed3b79d1be431e395cab43
Hannah McLaughlin <h@mcla.ug>
2016-08-11 16:20:22 +0100

    Forgot to add a return statement to one of the methods lol

--- chordal/pcp.py
+++ chordal/pcp.py
@@ -13,7 +13,7 @@ class ChordRecogniser:
         return self.f_s * bin_count / self.N
 
     def spectrum_bin_to_pcp_index(self, l, f_ref):
-        round(12 * np.log2(self.fft_bin_to_freq(l) / f_ref) % 12)
+        return round(12 * np.log2(self.fft_bin_to_freq(l) / f_ref) % 12)
 
     def pcp(self, pcp_index, f_ref):
         return sum(np.fft(l) for l in range(1, self.N/2 - 1)
