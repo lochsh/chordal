b2399f31ad89374c2ed94f27f7841f8aa4fedd40
Hannah McLaughlin <h@mcla.ug>
2016-09-08 14:55:44 +0100

    more name changes, some debugging

--- chordal/pcp.py
+++ chordal/pcp.py
@@ -8,41 +8,54 @@ class Chromagrammer:
     Calculates chromagrams, a.k.a. Pitch Class Profile (PCP) vectors
 
     Calculates chromagrams, 12 dimensional vectors indicating the relative
-    intensity of musical semitones across a time-domain signal.  The
-    chromagrams are calculated for a given sampling frequency and FFT length.
+    intensity of musical semitones across a time-domain signal.  A chroma or
+    pitch class refers to a note, e.g. C, regardless of tone height a.k.a
+    octave.
+
+    The chromagrams are calculated for a given sampling frequency and FFT
+    length.
     """
-    ref_freqs = {n: 2**(n/12) * 440.0 for n in range(12)}
+    ref_freqs = [2**(n/12) * 27.50 for n in range(12)]
 
     def __init__(self, sampling_freq, fft_length):
         self.f_s = sampling_freq
         self.N = fft_length
 
-    def fft_bin_to_freq(self, k):
-        return self.f_s * k / self.N
+    def spectrum_bin_to_chroma_index(self, k, f_ref):
+        """Translate spectrum bin k to chroma index"""
 
-    def spectrum_bin_to_pcp_index(self, k, f_ref):
-        return np.floor(12 * np.log2(self.fft_bin_to_freq(k) / f_ref) % 12)
+        def spectrum_bin_to_freq():
+            return self.f_s * k / self.N
 
-    def single_chroma(self, data, pcp_index):
+        return np.floor(12 * np.log2(spectrum_bin_to_freq() / f_ref) % 12)
+
+    def chroma_intensity(self, data, chroma_index):
         """
-        Calculates the PCP level for a particular pitch for a single sample.
+        Calculates the intensity of a particular pitch in a single time sample.
 
-        Calculates the PCP level for a given pitch, defined by the pcp_index
-        parameter, for a single time-domain sample.  Effectively, we are
-        summing the components of the DFT that correspond to this pitch.
+        Calculates the intensity of a given pitch, defined by the PCP index
+        provided, for a single time-domain sample.  Efftively we are summing
+        the components of the DFT the correspond to this pitch.
         """
         mapping = (abs(np.fft.fft(data))**2
                    for k in range(1, int(self.N/2 - 1))
-                   if self.spectrum_bin_to_pcp_index(
-                       k, Chromagrammer.ref_freqs[pcp_index]) == pcp_index)
+                   if self.spectrum_bin_to_chroma_index(
+                       k, Chromagrammer.ref_freqs[chroma_index]) == chroma_index)
         return sum(sum(mapping))
 
     def chromagram(self, data_frames):
-        """Calculate chromagrams for successive time-domain samples"""
+        """
+        Calculate chromagrams for successive time-domain samples
+
+        Calculates chromagrams for successive time-domain samples, yielding 12
+        dimensional vectors for each sample.  Each entry in the vector is the
+        relative intensity of a chroma (e.g. the note C), normalised to be
+        between 0 and 1.
+        """
         for frame in data_frames:
-            chroma = np.array([self.single_chroma(frame, c)
-                               for c in Chromagrammer.ref_freqs])
-            yield chroma/chroma.max()
+            chromagram = np.array([self.chroma_intensity(frame, c)
+                                   for c in range(12)])
+            yield chromagram/chromagram.max()
 
 
 class AudioProcessor:
--- tests/test_pcp.py
+++ tests/test_pcp.py
@@ -28,26 +28,26 @@ class TestOverlappingFrames:
 
 def test_ref_freqs():
     """Compare calculated semitone frequencies with a reference list"""
-    ref_freqs = [440.000, 466.164, 493.883, 523.251, 554.365,
-                 587.330, 622.254, 659.255, 698.456, 739.989,
-                 783.991, 830.609]
-    assert np.allclose(list(chordal.Chromagrammer.ref_freqs.values()),
+    ref_freqs = [27.5000, 29.1352, 30.8677, 32.7032,
+                 34.6478, 36.7081, 38.8909, 41.2034,
+                 43.6535, 46.2493, 48.9994, 51.9131]
+    assert np.allclose(chordal.Chromagrammer.ref_freqs,
                        ref_freqs)
 
 
 def test_chromagram_is_not_always_the_same():
     ap = chordal.AudioProcessor('')
     frames = ap.overlapping_frames()
-    chroma_calc = chordal.Chromagrammer(ap.f_s, 2048)
-    chroma = chroma_calc.chromagram(frames)
+    chromagrammer = chordal.Chromagrammer(ap.f_s, 2048)
+    chromagram = chromagrammer.chromagram(frames)
 
     def is_same():
         for _ in range(100):
-            yield (next(chroma) == next(chroma)).all()
+            yield (next(chromagram) == next(chromagram)).all()
     assert not all(is_same())
 
 
-def test_single_chroma_zero_for_zero_data():
-    chroma_calc = chordal.Chromagrammer(44100, 2048)
-    for n in chroma_calc.ref_freqs:
-        assert chroma_calc.single_chroma(np.zeros(100), n) == 0
+def test_chroma_intensity_zero_for_zero_data():
+    chromagrammer = chordal.Chromagrammer(44100, 2048)
+    for n in range(12):
+        assert chromagrammer.chroma_intensity(np.zeros(100), n) == 0
