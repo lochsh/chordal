78c16c9fd19394a4af9d43bd4da2a902d0799800
Hannah McLaughlin <the.banannah@gmail.com>
2016-09-11 13:11:26 +0100

    Successfully automated creating all 12 major and all 12 minor chords yas

--- generate_sounds.py
+++ generate_sounds.py
@@ -30,9 +30,26 @@ def create_chord(tone_files, output_file):
                                 .format(' -i '.join(tone_files), output_file)))
 
 
+def create_chords(input_dir, output_dir, major=True):
+    try:
+        os.makedirs(output_dir)
+    except OSError:
+        pass
+
+    third, name = (4, 'major') if major else (3, 'minor')
+
+    for i in range(12):
+        chord = [chroma_names[i % 12],
+                 chroma_names[(i+third) % 12],
+                 chroma_names[(i+7) % 12]]
+        create_chord([os.path.join(input_dir, '{}_5.wav'.format(note))
+                      for note in chord],
+                     os.path.join(output_dir,
+                                  '{0}_{1}.wav'.format(chord[0], name)))
+
+
 if __name__ == '__main__':
     output_dir = 'sounds'
     create_semitones(output_dir)
-    a_minor = ['A', 'C', 'E']
-    create_chord(['{0}/{1}_5.wav'.format(output_dir, chroma)
-                  for chroma in a_minor], os.path.join(output_dir, 'am.wav'))
+    create_chords(output_dir, os.path.join(output_dir, 'chords'))
+    create_chords(output_dir, os.path.join(output_dir, 'chords'), major=False)
