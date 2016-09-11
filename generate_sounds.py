ff9a27542bb9b912a1e8bf0417c201c194eb035f
Hannah McLaughlin <the.banannah@gmail.com>
2016-09-11 12:45:50 +0100

    Updated generate_sounds.py.  Was silly and thought it wasn't working.  I forgot I don't have super hearing so cannot hear sounds at 27.5Hz!  Also have function for creating a chord by overlaying tones.  Next step is to automate multiple chords

--- generate_sounds.py
+++ generate_sounds.py
@@ -1,11 +1,38 @@
+import os
+import shlex
 import subprocess
 
 import chordal
 
 chroma_names = ['A', 'B_flat', 'B', 'C', 'C_sharp', 'D',
                 'E_flat', 'E', 'F', 'F_sharp', 'G', 'A_flat']
-for freq, chroma, octave in zip(chordal.Chromagrammer.ref_freqs,
-                                chroma_names, range(7)):
-    subprocess.call('ffmpeg -f lavfi -i '
-                    'sine=frequency={0}:duration=5 {1}_{2}.wav'
-                    .format(freq, chroma, octave).split())
+
+
+def create_semitones(output_dir):
+    try:
+        os.makedirs(output_dir)
+    except OSError:
+        pass
+
+    for octave in range(8):
+        for freq, chroma in zip(chordal.Chromagrammer.ref_freqs, chroma_names):
+            subprocess.call(shlex.split('ffmpeg -f lavfi -i '
+                                        '"sine=frequency={0}:duration=5" '
+                                        '{1}/{2}_{3}.wav'
+                                        .format(freq * (octave + 1),
+                                                output_dir, chroma, octave)))
+
+
+def create_chord(tone_files, output_file):
+    subprocess.call(shlex.split('ffmpeg -i {0} -filter_complex '
+                                '"[0:a][1:a]amerge=inputs=2[aout]" '
+                                '-map "[aout]" {1}'
+                                .format(' -i '.join(tone_files), output_file)))
+
+
+if __name__ == '__main__':
+    output_dir = 'sounds'
+    create_semitones(output_dir)
+    a_minor = ['A', 'C', 'E']
+    create_chord(['{0}/{1}_5.wav'.format(output_dir, chroma)
+                  for chroma in a_minor], os.path.join(output_dir, 'am.wav'))
